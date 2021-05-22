c7772fc2e35764edf0d88bc74a9616a300a39f1a
maxwell <mxncoby@gmail.com>
2021-05-22 10:31:45 -0400

    adding tailwind css

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -12,7 +12,7 @@ import { formatUSD } from "../../utils/utils";
 export const HomeView = () => {
   const { marketEmitter, midPriceInUSD } = useMarkets();
   const { tokenMap } = useConnectionConfig();
-  const SRM_ADDRESS = 'SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt';
+  const SRM_ADDRESS = "SRMuApVNdxXokk5GT7XD5cUUgXMBCoAz2LHeuAoKWRt";
   const SRM = useUserBalance(SRM_ADDRESS);
   const SOL = useUserBalance(WRAPPED_SOL_MINT);
   const { balanceInUSD: totalBalanceInUSD } = useUserTotalBalance();
@@ -32,6 +32,46 @@ export const HomeView = () => {
   }, [marketEmitter, midPriceInUSD, tokenMap]);
 
   return (
+    <div className="relative bg-white overflow-hidden">
+      <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+        <div className="sm:text-center lg:text-left">
+          <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+            <span className="block text-indigo-600 xl:inline">Revewz</span>{" "}
+            <br></br>
+          </h1>
+          <h2 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+            <span className="block xl:inline">
+              Reviews by the consumer &amp; for the consumer
+            </span>
+          </h2>
+          <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+            RevewZ offers a decentralized review system hosted on the Solana
+            blockchain that uses both SLP tokens and NTFs to authenticate and
+            validate product purchases and reviews made by consumers.
+          </p>
+          <div className="mt-5 sm:mt-8 sm:flex sm:justify-center lg:justify-start">
+            <div className="rounded-md shadow">
+              <a
+                href="#"
+                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 md:py-4 md:text-lg md:px-10"
+              >
+                Get started
+              </a>
+            </div>
+            <div className="mt-3 sm:mt-0 sm:ml-3">
+              <a
+                href="#"
+                className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-indigo-700 bg-indigo-100 hover:bg-indigo-200 md:py-4 md:text-lg md:px-10"
+              >
+                Sign Up
+              </a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+
+    /* 
     <Row gutter={[16, 16]} align="middle">
       <Col span={24}>
         <h2>Your balances ({formatUSD.format(totalBalanceInUSD)}):</h2>
@@ -52,6 +92,6 @@ export const HomeView = () => {
       <Col span={24}>
         <div className="builton" />
       </Col>
-    </Row>
+    </Row> */
   );
 };
